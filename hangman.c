7aad2d3e237eb8fb9785a79f5ba0ab5d7c36f638
Brent Pappas <pappasbrent@gmail.com>
2017-11-16 21:15:16 -0500

    Almost done

--- hangman.c
+++ hangman.c
@@ -1,10 +1,18 @@
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
+// Number of words in wordbank.txt.
+// I would prefer a way to dynamically update this,
+// but for now it is required to harcode the number of words
+// in the wordbank into the top of the program
 #define NUM_WORDS 123
+// Maximum string length
 #define MAX_STRING_LENGTH 100
+// The number of letter in the alphabet
+// (for all the possible letters the player can guess)
 #define NUM_LETTERS_IN_ALPHA 26
 
 // Prints the hangman. The number of body parts displayed
@@ -87,7 +95,61 @@ void clear_screen()
     // as to what the code is actually doing.
 }
 
-// Assigns user input to a global variable, and capitalizes it
+// Checks if a character c is in a string s.
+// Returns a 1 if so, or a 0 if not.
+// I am aware that this could be achieved using the built-in
+// function strchr instead of making my own; however
+// I wanted to try and create that function myself
+// using what we learned in class about loops.
+// Also, my function differs in that it is optionally
+// case sensitive
+int char_in_string(char c, char *s, int caseSensitive)
+{
+    // Iterator variable
+    int i;
+    // Length of string to check
+    int sLength = strlen(s);
+
+    // If the characters are not case sensitive
+    // (as denoted by caseSensitive being 0), then
+    // c and s will both be made uppercase first before checking
+    if (!caseSensitive)
+    {
+        c = toupper(c);
+        strcpy(s, strupr(s));
+    }
+
+    // Iterates through the input string s
+    for (i = 0; i < sLength; i++)
+    {
+        // Returns a 1 as soon as a character matching c
+        // is found
+        if (s[i] == c)
+        {
+            return 1;
+        }
+    }
+
+    // If no character matching c is found, then
+    // 0 is returned instead
+    return 0;
+}
+
+// Appends a character to c to the end of a string s
+void append_char_to_string(char c, char *s)
+{
+    // Length of string to append c to
+    int sLength = strlen(s);
+
+    // Assigns what was previously the terminating string character
+    // to be the character c.
+    s[sLength] = c;
+    // Then, the terminating string character is shifted forward one place
+    // to accommodate for the new size of the string
+    s[sLength + 1] = '\0';
+}
+
+// Assigns player input to a global variable, and capitalizes it
 // for easier comparison
 void assign_string_input_to(char s[])
 {
@@ -167,16 +229,21 @@ int main()
     while (1)
     {
 
+        // Print the title
         print_title();
 
+        // Get player input
         assign_string_input_to(s);
 
+        // Prints "INVALID INPUT" if the player did not
+        // enter "START" or "QUIT"
         if (strcmp(s, "START") || strcmp(s, "QUIT"))
         {
             clear_screen();
             printf("INVALID INPUT\n");
         }
 
+        // If the player entered "START", then the game begins
         if (!strcmp(s, "START"))
         {
             clear_screen();
@@ -196,30 +263,93 @@ int main()
                 clear_screen();
                 print_hangman(numIncorrect);
                 printf("%s\n", currentWord);
-                printf("%s\n", currentConsolePhrase);
-                printf("%c\n", c);
-                scanf(" %c", &c);
-                if (strchr(currentWord, c))
+                // Prints the current phrase that the console should
+                // be displaying
+
+                // Iterates through the string, and checks if each
+                // of its letters have already been guessed
+                for (i = 0; i < strlen(currentWord); i++)
                 {
+                    // If the current letter that is being checked is in
+                    // the guessedLetters string, then it must have already
+                    // been guessed, and is printed
+                    if (char_in_string(currentWord[i], guessedLetters, 0))
+                    {
+                        printf("%c ", currentWord[i]);
+                    }
+                    // If note, then a blank line (underscore) is printed
+                    // instead so that the player knows how many letters there
+                    // are in the word
+                    else
+                    {
+                        printf("_ ");
+                    }
+                }
+                // Prints a new line afterward
+                printf("\n");
+
+                printf("%s\n", currentConsolePhrase);
+                // Prints the last letter that the player entered
+                printf("%c\n", c);
+                // Assigns the first character input to the value of c.
+                // Notice the leading white space before %c. THIS IS IMPORTANT!
+                // Also note that due to just how scanning for chars works in C,
+                // the player can enter multiple letters at once, and then their
+                // results will each be computed almost instantly. This allows
+                // a player to take a guess at the word, but if they have gotten any
+                // letters wrong in their guess, then their number of incorrect guesses
+                // will be incremented almost instantly as well. So a wrong guess may
+                // cause a player to instantly win or instantly lose
+                scanf(" %c", &c);
+
+                // First checks to see if the entered letter has already been guessed.
+                // If so, the loop is immediately restarted.
+                if (char_in_string(c, guessedLetters, 0))
+                {
+                    strcpy(currentConsolePhrase, "LETTER ALREADY GUESSED!");
+                    continue;
+                }
+
+                // Checks if the entered letter is in the word that
+                // the player is guessing
+                // if (strchr(currentWord, c))
+                if (char_in_string(c, currentWord, 0))
+                {
+                    // If so, then the console phrase is made positive
+                    // and numIncorrect is not incremented
                     strcpy(currentConsolePhrase, "CORRECT");
                 }
                 else
                 {
+                    // If not, then the console phrase is made negative
+                    // and numIncorrect is incremented by 1
                     strcpy(currentConsolePhrase, "WRONG");
                     numIncorrect++;
                 }
+
+                // Adds the entered letter to the guessedLetters array
+                append_char_to_string(c, guessedLetters);
             }
+            // When the game is over, the screen is cleared,
+            // "GAME OVER" is printed, the final state of the hangman
+            // is displayed, and the player is prompted to return to the
+            // title screen
             clear_screen();
             printf("GAME OVER\n");
             print_hangman(numIncorrect);
             printf("PRESS ENTER TO RETURN TO THE TITLE SCREEN\n");
-            do {
+            // This little do while loop allows the player to simply
+            // press enter to return to the title screen, without
+            // having to enter a value first
+            do
+            {
                 getchar();
-            }
-            while (getchar() == ' ');
+            } while (getchar() == ' ');
             clear_screen();
         }
 
+        // If the player entered "QUIT", then the
+        // game returns 0 and ends
         if (!strcmp(s, "QUIT"))
         {
             clear_screen();
